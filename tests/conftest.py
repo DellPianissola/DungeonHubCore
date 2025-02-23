51d1dfda0448e0fcca4110f4497b3708e5422bce
DellPianissola <Dell.pianissola@outlook.com>
2025-02-23 20:46:43 -0300

    Ajuste gitignore

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,5 @@
-import os
 import pytest
-import tempfile
 
-from django.conf import settings
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
@@ -12,20 +9,6 @@ from tests.factories.dharma import NatureFactory
 
 register(UserFactory)
 register(NatureFactory)
-
-
-@pytest.fixture(scope='function')
-def media_root():
-    # Cria um diretório temporário para testes
-    temp_dir = tempfile.mkdtemp()
-    settings.MEDIA_ROOT = temp_dir
-    yield temp_dir
-    # Limpa o diretório temporário após o teste
-    for root, dirs, files in os.walk(temp_dir, topdown=False):
-        for name in files:
-            os.remove(os.path.join(root, name))
-        for name in dirs:
-            os.rmdir(os.path.join(root, name))
 
 
 @pytest.fixture
